83b731c3ae44aca1e1235e577967c26a512e7d35
scouarn <scouarn.contact@gmail.com>
2020-10-26 12:22:30 +0100

    article feed

--- libs/feed.js
+++ libs/feed.js
@@ -1,10 +1,20 @@
 
+function initMenu() {
+  let menu = document.getElementById("menu");
+  let links = menu.getElementsByTagName("li")
+  console.log(links)
+
+}
+
 
 function initFeed(cat = "main") {
+  feed = document.getElementById("article_feed")
 
 }
 
 function updateFeed(cat) {
   //BROWSER HISTORY PUSH
+
+
 
 }
--- libs/menu.js
+++
@@ -1,8 +0,0 @@
-
-
-function initMenu() {
-  let menu = document.getElementById("menu");
-  let links = menu.getElementsByTagName("li")
-  console.log(links)
-
-}

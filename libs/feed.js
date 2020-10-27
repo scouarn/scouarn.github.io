4c3f16480473902a6b8282567d6b242ac30fa06d
scouarn <scouarn.contact@gmail.com>
2020-10-27 18:07:14 +0100

    big update part 2

--- libs/feed.js
+++ libs/feed.js
@@ -1,20 +1,52 @@
 
+function getCat() {
+  const params = new URLSearchParams(window.location.search);
+  return params.get("cat");
+}
+
+
 function initMenu() {
+
   //set the onclick function
-  $.each($("#menu li"), (index, elem)=> $(elem).click(
-    ()=>updateFeed($(elem).attr("cat"))
-  ))
+  $.each($("#menu li"), (index, elem)=> {
+    $(elem).click(()=>{
+      const cat = $(elem).attr("cat");
 
+      if (cat != getCat()) {
+        window.history.pushState("State", 'Title', '/?cat=' + cat);
+        showFeed(cat);
+      }
+
+    });
+  });
+
+  //set the history back function
+  window.onpopstate = (e)=>{
+    if(e.state) {
+      updateFeed();
+    }
+  };
 
 }
 
 
-function initFeed(cat = "main") {
-  updateFeed(cat)
+function updateFeed() {
+
+  //find cat
+  const cat = getCat();
+
+  //show cat
+  if (cat == null) {
+    showFeed("main");
+  }
+  else {
+    showFeed(cat);
+  }
 }
 
-function updateFeed(cat) {
-  //BROWSER HISTORY PUSH
+
+
+function showFeed(cat) {
 
   //remove the feed
   $("#article_feed").empty();
@@ -22,9 +54,10 @@ function updateFeed(cat) {
   //load the feed
   $.getJSON( "/articles/manifest.json", (obj) =>{
     $.each(obj[cat].files,(index,file)=> {
-      $('#article_feed').append($("<article></article>").addClass("box").load('/articles/'+obj[cat].folder+'/'+file))
+      $('#article_feed').append($("<article></article>").addClass("box").load('/articles/'+obj[cat].folder+'/'+file));
 
     });
   });
+
 
 }
